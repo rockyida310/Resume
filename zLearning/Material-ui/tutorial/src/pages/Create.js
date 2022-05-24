63fda28efd6ae6f70907149afb5e5784aaf0f207
dedic <rockyida310@gmail.com>
2022-05-24 18:39:03 +0530

    material-ui-Buttons

--- zLearning/Material-ui/tutorial/src/pages/Create.js
+++ zLearning/Material-ui/tutorial/src/pages/Create.js
@@ -1,30 +1,44 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@mui/material/Button";
+// import ButtonGroup from "@mui/material/ButtonGroup";
+import Container from '@mui/material/Container';
 
 export default function Create() {
   return (
-    <div>
-      {/* <Typography 
-        variant="h1"
-        color="primary"
-        align="center"
-      >
-        Create a new Note
-      </Typography> */}
-       {/* <Typography 
-        noWrap 
-        color="secondary"
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nihil consequuntur ratione porro adipisci magnam, a minima. Hic cum voluptate illo iste deleniti architecto earum delectus dolor error maiores? Voluptas omnis molestias vitae voluptatem, atque ipsam autem consequuntur rerum alias?
-      </Typography> */}
+    <Container>
       <Typography
         variant="h6"
         color="textSecondary"
         component="h2"
         gutterBottom
       >
-        Create a New Note 
+        Create a New Note
       </Typography>
-    </div>
-  )
+
+      {/* <Button
+        type="submit"
+        color="primary"
+        variant='outlined'
+      >
+        Submit
+      </Button> */}
+
+      {/* <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+      <Button
+        onClick={()=> console.log('You clicked me')}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        // disableElevation
+      >
+        Submit
+      </Button>
+      {/* disableElevation for no shadow */}
+    </Container>
+  );
 }
