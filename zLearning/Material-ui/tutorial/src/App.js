5154b668c15f9a0df253eebed66c4f899a7ed1b4
dedic <rockyida310@gmail.com>
2022-05-24 21:28:08 +0530

    material-ui-Custom-themes

--- zLearning/Material-ui/tutorial/src/App.js
+++ zLearning/Material-ui/tutorial/src/App.js
@@ -1,19 +1,40 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import purple from '@mui/material/colors/purple'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple
+    //applied to dark,main & light automatically
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightDark: 700,
+  }
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- zLearning/Material-ui/tutorial/src/pages/Create.js
+++ zLearning/Material-ui/tutorial/src/pages/Create.js
@@ -1,35 +1,20 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
+import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { makeStyles } from "@material-ui/styles";
+// import { makeStyles } from "@material-ui/styles";
 
-const useStyle = makeStyles({
-  title: {
-    textDecoration: 'underline',
-    marginBottom: 20
-  }
-});
+// const useStyle = makeStyles({
 
-//makeStyles not working on button so using custom styles
-const style={
-  btn : {
-    fontSize: 60,
-    backgroundColor: 'violet',
-    '&:hover' :{
-      backgroundColor: 'blue'
-    }
-  }
-}
+// });
 
 
 export default function Create() {
-  const classes = useStyle();
+  // const classes = useStyle();
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant="h6"
         color="textSecondary"
         component="h2"
@@ -39,7 +24,6 @@ export default function Create() {
       </Typography>
 
       <Button
-        sx={style.btn}
         onClick={() => console.log("You clicked me")}
         type="submit"
         color="secondary"
