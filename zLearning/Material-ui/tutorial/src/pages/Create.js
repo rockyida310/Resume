311af53b567e75032ec8498bd5d5807757404b44
dedic <rockyida310@gmail.com>
2022-05-28 14:34:01 +0530

    2-useState-basics

--- zLearning/Material-ui/tutorial/src/pages/Create.js
+++ zLearning/Material-ui/tutorial/src/pages/Create.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-// import { makeStyles } from "@material-ui/styles";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { TextField } from "@mui/material";
+import { makeStyles, createStyles } from "@mui/styles";
 
-// const useStyle = makeStyles({
-
-// });
-
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    field: {
+      marginBottom: 20,
+      display: "block",
+    },
+  })
+);
 
 export default function Create() {
-  // const classes = useStyle();
+  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  // console.log(`title : ${title}`);
+  // console.log(`details : ${details}`);
+
+  const handleSubmit = (e) => {
+    // e.preventDeafault();
+    console.log(title + "\n" + details);
+  };
+
   return (
     <Container>
       <Typography
@@ -23,19 +39,41 @@ export default function Create() {
         Create a New Note
       </Typography>
 
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          InputProps={{ className: classes.field }}
+          margin="dense"
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required //require just add * and not some validation
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          InputProps={{ className: classes.field }}
+          margin="dense"
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          multiline
+          rows={4}
+          required //require just add * and not some validation
+        />
+      </form>
       <Button
-        onClick={() => console.log("You clicked me")}
+        onClick={(e) => {
+          handleSubmit(e);
+        }}
         type="submit"
         color="secondary"
         variant="contained"
-        endIcon={<KeyboardArrowRightIcon/>}
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
-
-      <br/>
-
-
     </Container>
   );
 }
