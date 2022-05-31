9e84a08dfbc799f400f997d9f4ec9ab2ea4cd622
dedic <rockyida310@gmail.com>
2022-05-31 20:32:36 +0530

    useRef

--- zLearning/React/Learning/Advanced/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ zLearning/React/Learning/Advanced/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,37 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+    divContainer.current.innerText = "Submitted";
+    divContainer.current.style.fontWeight = "bold";
+  }
+
+  //no need to pass dependency array beacuse there will be no rerender 
+  //as we are not using useState 
+  useEffect(()=>{
+      console.log(refContainer.current);
+      refContainer.current.focus();
+  });
+
+  return(
+    <>
+      <form className='form' onSubmit={handleSubmit}>
+        <div> 
+          <input type="text" ref={refContainer} />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div ref={divContainer}>
+        Please Fill Up
+      </div>
+    </>
+  )
 };
 
 export default UseRefBasics;
