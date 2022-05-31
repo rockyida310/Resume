8112b6dac40da5477380b4a2bc66d13579df8636
dedic <rockyida310@gmail.com>
2022-05-31 22:49:18 +0530

    Reviews project completed

--- zLearning/React/Projects/react-projects/03-reviews/setup/src/App.js
+++ zLearning/React/Projects/react-projects/03-reviews/setup/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import Review from './Review';
+// import {FaGithubSquare} from "react-icons/fa";
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <>
+      {/* <h2>Reviews</h2> */}
+      {/* <FaGithubSquare className='icon'/> */}
+      <main>
+        <section className="container">
+          <div className="title">
+            <h2>our Reviews</h2>
+            <div className="underline"></div>
+          </div>
+          <Review/>
+        </section>
+      </main>
+    </>
+   
+  )
 }
 
 export default App;
