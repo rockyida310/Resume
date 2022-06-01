27ce33801ebabb034666fe6e48c612beb8d0d826
dedic <rockyida310@gmail.com>
2022-06-01 23:16:08 +0530

    Menu Project Completed

--- zLearning/React/Projects/react-projects/05-menu/setup/src/App.js
+++ zLearning/React/Projects/react-projects/05-menu/setup/src/App.js
@@ -1,10 +1,36 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ['all',...new Set(items.map((item)=> item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allCategories);
+
+  const filterItem = (category) => {
+    if(category === 'all'){
+      setMenuItems(items);
+      return;
+    }
+    //items is the original list from "./data"
+    const newItems = items.filter((item)=>item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItem={filterItem} />
+        <Menu items={menuItems}/>
+      </section>
+    </main>
+  );
 }
 
 export default App;
